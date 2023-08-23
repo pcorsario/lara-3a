a440ae84b0a6a531358ae6486ed3d6d1a178e396
pcorsario <patriciovacaescobar@gmail.com>
2023-08-23 12:30:15 -0500

    Add function edit of students

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -54,12 +54,14 @@ shadow-lg block md:inline-block">Agregar Nuevo</a>
 
             <td
                 class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+               <a href="{{ route('students.edit',$student->id)}}"> 
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-blue-400"
                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         stroke-width="2"
                         d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                 </svg>
+            </a>
             </td>
             <td
                 class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
